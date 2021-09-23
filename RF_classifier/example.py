26433129fe4a6923533e9539ff8763e82223f27e
HAJJMM <KAUST@03.08.1986>
2021-09-24 02:38:04 +0300

    add ndvi stats, remove normalisation 0-1, remove decomp, refacto

--- RF_classifier/example.py
+++ RF_classifier/example.py
@@ -1,22 +1,50 @@
 import numpy as np
 import pandas as pd
 
-from RF_classifier.common import smooth_variables, gauss_decomp, normalise_cols
+from RF_classifier.common import smooth_variables, normalise_cols
 from RF_classifier.features import generate_features
 from info import outputs
 
 
-# if __name__ == '__main__':
-#     # get example
-#     _, df_query, _ = get_example()
-#     df_template = pd.read_csv(outputs / 'template.csv', sep=',')
-#     df = dtw_compute(df_query, 'name', ['VV_dB', 'VH_dB'], df_template, 'ref_class')
-
-
 def get_example():
-    # get stat and prepare features
+    # Prepare for features
+    # read SAR data
     stats = pd.read_csv(outputs / 'stats/hand_map.csv', sep=',', parse_dates=['image_date_time_ksa'])
     stats = stats.loc[stats['image_date_time_ksa'].between('2014-11-01', '2015-12-01')]
+
+    # add the ndvi to SAR data date by interpolation
+    ndvi = pd.read_csv(outputs / 'stats/hand_map_ndvi.csv', sep=',', parse_dates=['image_date_time_ksa'])
+    ndvi = ndvi[['image_date_time_ksa', 'name', 'ndvi']]
+    # to create nan when the NDVO do not match SAR time
+    ndvi = ndvi.merge(stats[['image_date_time_ksa', 'name']], left_on=['image_date_time_ksa', 'name'],
+                      right_on=['image_date_time_ksa', 'name'], how='outer')
+    # sort value and add inted to interpolate
+    ndvi = ndvi.sort_values(['name', 'image_date_time_ksa'])
+    ndvi = ndvi.set_index('image_date_time_ksa')
+
+    all_df = []
+    for name, sdf in ndvi.groupby(by=['name']):
+        sdf = sdf.interpolate(method='linear')
+        all_df.append(sdf)
+    all_df = pd.concat(all_df)
+    all_df = all_df.reset_index()
+
+    # join interpolated ndvi and SAR
+    prev_nb = len(stats)
+    stats = all_df.merge(stats, left_on=['name', 'image_date_time_ksa'], right_on=['name', 'image_date_time_ksa'],
+                         how='inner')
+
+    assert len(stats) == prev_nb
+
+    # delete non cultivated plots
+    all_df = []
+    for name, sdf in stats.groupby(by=['name']):
+        max_ndvi = sdf['ndvi'].max()
+        if max_ndvi > 0.3:
+            all_df.append(sdf)
+    all_df = pd.concat(all_df)
+    stats = all_df.reset_index()
+
     stats = stats[stats['type_1st_h'] != 'not classified']
     stats = stats[stats['type_1st_h'] != 'fruit']
     stats = stats[stats['type_1st_h'] != 'olive']
@@ -28,12 +56,14 @@ def get_example():
     stats = stats.sort_values(['name', 'image_date_time_ksa'])
 
     # generate more raw variables
-    df = smooth_variables(stats, ['name', 'inc_class', 'year'], ['VV_dB', 'VH_dB', 'VV_VH_dB'], 2)
+    df = smooth_variables(stats, ['name', 'inc_class', 'year'], ['VV_dB', 'VH_dB', 'VV_VH_dB', 'ndvi'], 2)
 
-    # scale data between 0 and 1
-    df = normalise_cols(df, ['VV_dB', 'VH_dB', 'VV_dB_smooth', 'VH_dB_smooth', 'VV_VH_dB_smooth'], 'name')
+    # # scale data between 0 and 1
+    # df = normalise_cols(df, ['VV_dB', 'VH_dB', 'VV_dB_smooth', 'VH_dB_smooth', 'VV_VH_dB_smooth', 'ndvi_smooth'],
+    #                     'name')
 
-    df = df[['name', 'VV_dB_smooth', 'VH_dB_smooth', 'VV_VH_dB_smooth', 'type_1st_h', 'image_date_time_ksa']]
+    df = df[
+        ['name', 'VV_dB_smooth', 'VH_dB_smooth', 'VV_VH_dB_smooth', 'type_1st_h', 'image_date_time_ksa', 'ndvi_smooth']]
 
     # # decompose into gauss
     # df = gauss_decomp(df, ['VV_dB_smooth', 'VH_dB_smooth', 'VV_VH_dB_smooth'], 'name')
@@ -43,8 +73,9 @@ def get_example():
     # dtw = dtw_compute(df, 'name', ['VV_dB_smooth', 'VH_dB_smooth'], df_template, 'ref_class')
 
     # get features for all segments
-    features = generate_features(df, 'name', 'type_1st_h', ['VV_dB_smooth', 'VH_dB_smooth', 'VV_VH_dB_smooth'],
-                                 'image_date_time_ksa')
+    features = generate_features(df, 'name', 'type_1st_h',
+                                 ['VV_dB_smooth', 'VH_dB_smooth', 'VV_VH_dB_smooth', 'ndvi_smooth'],
+                                 'image_date_time_ksa', clos_cmp=['VV_dB_smooth', 'VH_dB_smooth'])
 
     # features = features.merge(dtw, left_on='label', right_on='label')
 
--- viualization/plot_temporal.py
+++ viualization/plot_temporal.py
@@ -7,7 +7,9 @@ from viualization.common import plot_temporal_evolution
 # get example
 _, df, _ = get_example()
 
-fig, axes = plt.subplots(ncols=3, nrows=6, figsize=(15, 15), sharex='all', sharey='all')
+# df = df[df['name'] == 'B5']
+
+fig, axes = plt.subplots(ncols=4, nrows=6, figsize=(15, 15), sharex='all')
 # axes = axes.flatten()
 
 for id, (name, sdf) in enumerate(df.groupby(by=['type_1st_h'])):
@@ -17,18 +19,23 @@ for id, (name, sdf) in enumerate(df.groupby(by=['type_1st_h'])):
         vv_smooth_db = s_sdf['VV_dB_smooth']
         vh_smooth_db = s_sdf['VH_dB_smooth']
         vv_vh_smooth_db = s_sdf['VV_VH_dB_smooth']
+        ndvi = s_sdf['ndvi_smooth']
         # plot = sdf['ref_hand'].unique()[0]
 
         # plot for VV
         plot_temporal_evolution(x=date, y=vv_smooth_db, ax=axes[id, 0], y_label='', text_font_size=10,
-                                xylabel_font_size=30, marker=None, ls='-', ylim=None, title=name)
+                                xylabel_font_size=30, marker=None, ls='-', ylim=[-19, -9], title=name)
 
         plot_temporal_evolution(x=date, y=vh_smooth_db, ax=axes[id, 1], y_label='', text_font_size=10,
-                                xylabel_font_size=30, marker=None, ls='-', ylim=None, title=name)
+                                xylabel_font_size=30, marker=None, ls='-', ylim=[-25, -10], title=name)
 
         plot_temporal_evolution(x=date, y=vv_vh_smooth_db, ax=axes[id, 2], y_label='', text_font_size=10,
-                                xylabel_font_size=30, marker=None, ls='-', ylim=None, title=name)
-        axes[id, 0].text(date.values[-1], vv_smooth_db.values[-1], plot_id)
+                                xylabel_font_size=30, marker=None, ls='-', ylim=[5, 15], title=name)
+
+        plot_temporal_evolution(x=date, y=ndvi, ax=axes[id, 3], y_label='', text_font_size=10,
+                                xylabel_font_size=30, marker=None, ls='-', ylim=[0, 1], title=name)
+
+        # axes[id, 0].text(date.values[-1], vv_smooth_db.values[-1], plot_id)
 plt.savefig(f"{outputs}/figures/graph.png", bbox_inches='tight', pad_inches=0.1)
 plt.show()
 plt.close()
